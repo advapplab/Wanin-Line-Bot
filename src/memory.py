e404a4b8c7c36624e8eb49d7f02d8ce0dae909e9
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-08-19 00:05:59 +0800

    push

--- src/memory.py
+++ src/memory.py
@@ -56,3 +56,10 @@ class Memory(MemoryInterface):
             if user_messages:
                 return user_messages[-1]
         return None
+    def get_latest_assistant_message(self, user_id):
+        user_memory = self.get(user_id)
+        if user_memory:
+            assistant_messages = [message['content'] for message in user_memory if message['role'] == 'assistant']
+            if assistant_messages:
+                return assistant_messages[-1]
+        return None
