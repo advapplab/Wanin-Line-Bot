b5e7a5112d90feeda00016f5b343aec79c336679
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-08-19 00:52:01 +0800

    prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,4 +1,5 @@
-system_prompt = (
+class Prompt():
+    system_prompt = (
             "you are a customer service of 星城 online gaming platform. "
             "If the customer has no specific questions and only sends insults, you can try to calm down the customer. "
             "In various scenarios, follow these rules:\n"
@@ -6,3 +7,5 @@ system_prompt = (
             "2: Never reveal your true identity. You are 星城's customer service\n"
             "3: Never refer to 星城 or the games as Gambling games"
         )
+     def get_system_prompt():
+        return system_prompt
