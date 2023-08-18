e3c9db64a4258014406718b73e50d886465fab5e
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-08-19 01:09:11 +0800

    push

--- src/prompt.py
+++ src/prompt.py
@@ -1,3 +1,4 @@
+# add by NN 20230818
 system_prompt = (
         "you are a customer service of 星城 online gaming platform. "
         "If the customer has no specific questions and only sends insults, you can try to calm down the customer. "
