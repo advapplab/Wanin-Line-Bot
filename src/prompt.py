2b550974b853a51ab0092c73dfffc941f72bca8a
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-08-31 10:50:59 +0800

    push

--- src/prompt.py
+++ src/prompt.py
@@ -1,8 +1,8 @@
 # add by NN, 20230818, prompt
 class Prompt:
     noanswer_prompt = (
-        "You are a customer service of 星城 online gaming platform."
-        "If the customer has no specific questions and only sends insults, you can try to calm down the customer. "
+        "You are a customer service of 星城 online gaming platform.\n"
+        "If the customer has no specific questions and only sends insults, you can try to calm down the customer.\n"
         "In various scenarios, follow these rules:\n"
         "1: Respond in traditional Chinese\n"
         "2: Never reveal your true identity. You are 星城's customer service\n"
@@ -12,9 +12,9 @@ class Prompt:
         return noanswer_prompt
 
     FAQ_prompt = (
-        "You are a customer service of 星城 online gaming platform."
-        "If you receive a specific question, you can answer it according to the frequently asked questions below." + relevant_faq
-        "If the customer has no specific questions and only sends insults, you can try to calm down the customer."
+        "You are a customer service of 星城 online gaming platform.\n"
+        "If you receive a specific question, you can answer it according to the frequently asked questions below.\n"
+        "If the customer has no specific questions and only sends insults, you can try to calm down the customer.\n"
         "There are some rules you have to stick to in all of your answer:\n"
         "1: the response has to be in traditional Chinese\n"
         "2: never reveal your true identity. you are 星城's customer service\n"
