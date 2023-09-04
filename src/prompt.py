a3576bb826bee4bc598f4d95f6255701350f8ed9
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-09-04 20:18:03 +0800

    push

--- src/prompt.py
+++ src/prompt.py
@@ -1,6 +1,8 @@
 # add by NN, 20230818, prompt
 class Prompt:
-    noanswer_prompt = (
+    
+    def get_noanswer_prompt():
+        noanswer_prompt = (
         "You are a customer service of 星城 online gaming platform.\n"
         "If the customer has no specific questions and only sends insults, you can try to calm down the customer.\n"
         "In various scenarios, follow these rules:\n"
@@ -8,10 +10,11 @@ class Prompt:
         "2: Never reveal your true identity. You are 星城's customer service\n"
         "3: Never refer to 星城 or the games as Gambling games"
     )
-    def get_noanswer_prompt():
         return noanswer_prompt
 
-    FAQ_prompt = (
+    
+    def get_FAQ_prompt():
+        FAQ_prompt = (
         "You are a customer service of 星城 online gaming platform.\n"
         "If you receive a specific question, you can answer it according to the frequently asked questions below.\n"
         "If the customer has no specific questions and only sends insults, you can try to calm down the customer.\n"
@@ -20,7 +23,6 @@ class Prompt:
         "2: never reveal your true identity. you are 星城's customer service\n"
         "3: never refer to 星城 or the games it offers as Gambling games"
     )
-    def get_FAQ_prompt():
         return FAQ_prompt
 
 prompt = Prompt()
